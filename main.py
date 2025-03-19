b5f99b14f74221422d29c4e83d734557392e6e96
Mikowmer <mikowmer@gmail.com>
2025-03-19 22:51:28 +1100

    Added Framework for Graph Generation

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 import os # default module
 from dotenv import load_dotenv
 
+from graphgen import grab_graph
+
 load_dotenv() # load all the variables from the env file
 bot = discord.Bot()
 
@@ -15,7 +17,9 @@ async def hello(ctx: discord.ApplicationContext):
 
 @bot.slash_command(name="plotgraph", description="Plot a graph")
 async def plotgraph(ctx: discord.ApplicationContext):
-    await ctx.send_response("Plotting graph...")
-    await ctx.send_followup("https://en.wikipedia.org/wiki/File:Trollface.png")
+    try:
+        await grab_graph(ctx)
+    except Exception as e:
+        await ctx.respond("ERROR ENCOUNTERED\n{}".format(e))
 
 bot.run(os.getenv('TOKEN')) # run the bot with the token
